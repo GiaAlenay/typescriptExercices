9c15bd6d1bac1b4837121be294b0dec144680951
Gianella Leon <giigl@hotmail.com>
2023-03-27 21:57:51 -0500

    extra 2

--- ejer.ts
+++ ejer.ts
@@ -152,7 +152,7 @@ return [[1,2],[4,5]]
 console.log(ejer5())
  
 
-
+//ejer 6
 
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
 
@@ -161,3 +161,17 @@ console.log(ejer5())
 // Return k after placing the final result in the first k slots of nums.
 
 
+
+
+
+
+//exercise 7
+// 27. Remove Element
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
